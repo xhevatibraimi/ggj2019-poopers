85deb5dd43fe87b7d85303e0b0c22277a4f9e9ea
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2019-01-27 06:23:00 +0100

    added you pooped image

--- Poopers/Assets/Scripts/UIManager.cs
+++ Poopers/Assets/Scripts/UIManager.cs
@@ -4,12 +4,13 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MallUIManager : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     public Text ScoreText;
     public Text GameOverText;
     public Button RestartButton;
     public Sprite RestartButtonSprite;
+    public Image YouPoopedImage;
     static bool created = false;
 
     void Awake()
@@ -32,6 +33,7 @@ public class MallUIManager : MonoBehaviour
         RestartButton.onClick.AddListener(RestartGame);
         RestartButton.image.sprite = RestartButtonSprite;
         RestartButton.image.color = new Color(RestartButton.image.color.r, RestartButton.image.color.g, RestartButton.image.color.b, 0);
+        YouPoopedImage.color = new Color(YouPoopedImage.color.r, YouPoopedImage.color.g, YouPoopedImage.color.b, 0);
     }
 
     private void OnGameOver(object sender, EventArgs e)
@@ -39,6 +41,7 @@ public class MallUIManager : MonoBehaviour
         GameOverText.text = "GAME OVER";
         RestartButton.image.sprite = RestartButtonSprite;
         RestartButton.image.color = new Color(RestartButton.image.color.r, RestartButton.image.color.g, RestartButton.image.color.b, 255);
+        YouPoopedImage.color = new Color(YouPoopedImage.color.r, YouPoopedImage.color.g, YouPoopedImage.color.b, 255);
     }
 
     private void ChanfgeScore(object sender, ScoreChanged e)
@@ -51,6 +54,7 @@ public class MallUIManager : MonoBehaviour
         RestartButton.image.sprite = null;
         GameOverText.text = string.Empty;
         RestartButton.image.color = new Color(RestartButton.image.color.r, RestartButton.image.color.g, RestartButton.image.color.b, 0);
+        YouPoopedImage.color = new Color(YouPoopedImage.color.r, YouPoopedImage.color.g, YouPoopedImage.color.b, 0);
         ScoreText.text = "0";
         GameManager.Instance.OnRestartGame.Invoke(this, new EventArgs());
     }
